c5dcd862b137c325df21702dfcc10a050d9d1235
Marko MIlkovic <mmilkovic2006@gmail.com>
2025-08-25 21:52:08 +0200

    Updated some small stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -45,16 +45,24 @@ const scrollIndicator = {
         :class="{ 'bg-[#021515] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] z-20': scrollPosition > 0 }"
       >
         <div>
-          <RouterLink to="/">Početna</RouterLink>
+          <a href="#" class="hover:text-[#fff] transition duration-300 ease-in-out">Početna</a>
         </div>
         <div class="flex space-x-8">
-          <RouterLink to="/about">O meni</RouterLink>
-          <RouterLink to="/pricing">Cjenik</RouterLink>
-          <RouterLink to="/reviews">Recenzije</RouterLink>
-          <RouterLink to="/services">Usluge</RouterLink>
+          <a href="#about" class="hover:text-[#fff] transition duration-300 ease-in-out">O meni</a>
+          <RouterLink to="/" class="hover:text-[#fff] transition duration-300 ease-in-out"
+            >Cjenik</RouterLink
+          >
+          <RouterLink to="/reviews" class="hover:text-[#fff] transition duration-300 ease-in-out"
+            >Recenzije</RouterLink
+          >
+          <a href="#services" class="hover:text-[#fff] transition duration-300 ease-in-out"
+            >Usluge</a
+          >
         </div>
         <div>
-          <RouterLink to="/contact">Kontakt</RouterLink>
+          <RouterLink to="/contact" class="hover:text-[#fff] transition duration-300 ease-in-out"
+            >Kontakt</RouterLink
+          >
         </div>
       </motion.div>
       <div class="relative flex-1 flex flex-col items-center justify-center space-y-4 w-full">
@@ -71,7 +79,7 @@ const scrollIndicator = {
               :key="i"
               :initial="{ opacity: 0 }"
               :animate="{ opacity: 1, transition: { delay: i * 0.04 } }"
-              class="font-cinzel text-white text-7xl tracking-tight inline-block"
+              class="font-cinzel text-white text-7xl -tracking-widest inline-block"
               >{{ char === " " ? "\u00A0" : char }}</motion.h1
             >
           </div>
@@ -79,19 +87,20 @@ const scrollIndicator = {
         <motion.p
           :initial="{ opacity: 0, y: 20 }"
           :animate="{ opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.7 } }"
-          class="text-lighter-white font-light text-2xl w-1/3 text-center"
-          >Pomažem ženama da imaju savršene obrve, senzualne usne i zavodljive oči.</motion.p
+          class="text-lighter-white font-light text-2xl text-center"
+          >Pomažem ženama da imaju savršene obrve,<br />
+          senzualne usne i zavodljive oči.</motion.p
         >
         <motion.p
           :initial="{ opacity: 0, y: 20 }"
           :animate="{ opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.7 } }"
-          class="flex gap-1 text-white font-light items-center text-xl mt-4"
+          class="flex gap-1 text-white items-center font-light text-xl mt-4"
           ><FontAwesomeIcon :icon="faLocationDot" class="text-[#8D3535] text-3xl" /> Slatina 50,
           44250 Petrinja</motion.p
         >
       </div>
     </div>
-    <div class="bg-[#151919] pt-20 pb-16 px-16 flex">
+    <div class="bg-[#151919] pt-20 pb-16 px-16 flex" id="about">
       <motion.div
         class="flex flex-col w-1/2 space-y-2 justify-center"
         :initial="{ filter: 'blur(2px)', opacity: 0 }"
@@ -104,8 +113,9 @@ const scrollIndicator = {
           </h2>
           <div class="w-[75%] h-[1px] mt-4 bg-[#919191]"></div>
         </div>
-        <p class="font-light text-3xl leading-7 my-2">
-          MS Beauty & Brows se od 2020. godine specijalizirao za vrhunske tretmane microbladinga,
+        <p class="font-extralight text-3xl leading-7 my-2">
+          MS Beauty & Brows se od 2020. godine<br />
+          specijalizirao za vrhunske tretmane microbladinga,<br />
           trajne šminke, tetoviranja i trepavica.
         </p>
         <div class="flex flex-col space-y-4 mt-12 text-2xl">
@@ -129,11 +139,11 @@ const scrollIndicator = {
         :in-view-options="{ once: true }"
         class="flex flex-1 flex-col items-center justify-between gap-4"
       >
-        <img src="/images/marina-about.png" alt="Marina Šimunić" width="500" />
+        <img src="/images/marina-about.png" alt="Marina Šimunić" class="max-w-[500px] w-auto" />
         <p class="text-3xl text-[#A5A5A5] font-crimson-text tracking-wide">mag. med. techn.</p>
       </motion.div>
     </div>
-    <div class="w-full h-[1px] bg-[#555]"></div>
+    <div class="w-full h-[1px] bg-[#555]" id="services"></div>
     <ServiceComponent />
   </div>
 </template>
